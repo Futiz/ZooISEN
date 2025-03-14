dc2ca07bd8069396230c2111cee4f09e4985d1ae
Chutobu <tombouchut18@gmail.com>
2025-03-14 11:56:29 +0100

    remerge des deux branches

--- app/src/main/java/fr/isen/sannicolas/zooisen/MainActivity.kt
+++ app/src/main/java/fr/isen/sannicolas/zooisen/MainActivity.kt
@@ -5,19 +5,15 @@ import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.navigation.compose.rememberNavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import fr.isen.sannicolas.zooisen.screens.BiomeScreen
-import fr.isen.sannicolas.zooisen.screens.EnclosureScreen
-import fr.isen.sannicolas.zooisen.screens.ParkServiceScreen
 import fr.isen.sannicolas.zooisen.screens.EnclosureCommentScreen
+import fr.isen.sannicolas.zooisen.screens.EnclosureScreen
 import fr.isen.sannicolas.zooisen.screens.ParkMapScreen
+import fr.isen.sannicolas.zooisen.screens.ParkServiceScreen
 import fr.isen.sannicolas.zooisen.ui.theme.ZooISENTheme
 
 class MainActivity : ComponentActivity() {
